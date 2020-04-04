2a95351e1c2e48fae06b72d1d23693a08d033dd0
AdmiralGajeeban <42116539+AdmiralGajeeban@users.noreply.github.com>
2020-04-04 17:08:18 +0100

    Add files via upload

--- Droid.java
+++ Droid.java
@@ -21,7 +21,18 @@ public class Droid {
     //Updating the battery after a task
     public void updateBattery(int Level){
         batteryLevel = Level - 10;
+    }
 
+    //Transfering battery power from one droid to another
+
+    public int energyTransfer (int x, int y){
+        int a = x + y;
+        return a;
+    }
+
+    public int energyLoss (int x, int y){
+        int a = x - y;
+        return a;
     }
 
     public String toString(){
@@ -36,7 +47,20 @@ public class Droid {
         System.out.println(droid.toString());
         droid.performTask(droid.name, "singing");
         droid.updateBattery(droid.batteryLevel);;
-        System.out.println("Updated battery level is now " +droid.batteryLevel);
+        droid.performTask(droid.name, "dancing");
+        droid.updateBattery(droid.batteryLevel);;
+        System.out.println("Updated battery level for " +droid.name+ " is now " +droid.batteryLevel);
+
+
+        Droid droid2 = new Droid ("Hisoka");
+        System.out.println(droid2.toString());
+
+        int update = droid.energyTransfer(droid.batteryLevel, droid2.batteryLevel);
+        int update2 = droid2.energyLoss(droid2.batteryLevel, 10);
+        System.out.println("Energy transferred to " + droid.name + " from " + droid2.name +". " + droid.name +" now has " + droid.batteryLevel +" and " + droid2.name+" has " +droid2.batteryLevel);
+
+
+
     }
 
     }
